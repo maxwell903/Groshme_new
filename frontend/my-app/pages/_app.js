fe3d0f97a2d1a69459c531d0bb753adbc670b9a5
maxwell903 <maxwayne903@gmail.com>
2025-01-13 16:57:47 -0500

    test

--- frontend/my-app/pages/_app.js
+++ frontend/my-app/pages/_app.js
@@ -1,13 +1,13 @@
-// pages/_app.js
 import { useState } from 'react';
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
-import { useRouter } from 'next/router';
-import { supabase } from '../src/lib/supabaseClient';  // Updated path
 import '@/styles/globals.css';
 
 function App({ Component, pageProps }) {
-  const [supabaseClient] = useState(() => createPagesBrowserClient());
+  const [supabaseClient] = useState(() => createPagesBrowserClient({
+    supabaseUrl: 'https://bvgnlxznztqggtqswovg.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2Z25seHpuenRxZ2d0cXN3b3ZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5MDI1ODIsImV4cCI6MjA1MDQ3ODU4Mn0.I8alzEBJYt_D1PDZHvuyZzLzlAEANTGkeR3IRyp1gCc'
+  }));
 
   return (
     <SessionContextProvider
