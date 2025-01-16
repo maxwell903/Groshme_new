5c83a2bbf56f46ca49973e4d838fc315b5a390f9
maxwell903 <maxwayne903@gmail.com>
2025-01-16 00:16:26 -0500

    test

--- frontend/my-app/pages/middleware.js
+++ frontend/my-app/pages/middleware.js
@@ -1,4 +1,4 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { createMiddlewareClient } from '@supabase/supabase-js';
 import { NextResponse } from 'next/server';
 
 export async function middleware(req) {
