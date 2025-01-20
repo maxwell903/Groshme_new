bbffe6244aca0ef35f4fd4e9366e66450f70baf8
maxwell903 <maxwayne903@gmail.com>
2025-01-19 19:39:18 -0500

    test

--- frontend/my-app/src/utils/api.js
+++ frontend/my-app/src/utils/api.js
@@ -1,6 +1,7 @@
 
 import { supabase } from '@/lib/supabaseClient';
 
+export const API_URL = 'https://groshmebeta-05487aa160b2.herokuapp.com';
 export const fetchApi = async (endpoint, options = {}) => {
   const session = await supabase.auth.getSession();
   const token = session?.data?.session?.access_token;
