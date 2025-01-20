7448be68fa7f294c84d2996d018544a5bae585e2
maxwell903 <maxwayne903@gmail.com>
2025-01-19 19:21:56 -0500

    test

--- frontend/my-app/pages/all-recipes.js
+++ frontend/my-app/pages/all-recipes.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { debounce } from 'lodash';
 import { fetchApi, API_URL } from '@/utils/api';
-import ProtectedRoute from '../components/ProtectedRoute';
+import ProtectedRoute from '@/components/ProtectedRoute';
 
 const RecipeNavigation = ({ activePage }) => {
   return (
