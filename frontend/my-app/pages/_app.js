d9fb15ff06518d49a6e695a979b024c8bbe58dc2
maxwell903 <maxwayne903@gmail.com>
2025-01-19 19:41:53 -0500

    test

--- frontend/my-app/pages/_app.js
+++ frontend/my-app/pages/_app.js
@@ -1,14 +1,17 @@
 import { useState } from 'react';
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { AuthProvider } from '../contexts/AuthContext';
 import '@/styles/globals.css';
-import { AuthProvider } from '../src/components/contexts/AuthContext';
 
-function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   const [supabaseClient] = useState(() => createPagesBrowserClient());
 
   return (
-    <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
