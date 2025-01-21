7e7a113c91b9b7b185ed8f231366e96bf51c33e7
maxwell903 <maxwayne903@gmail.com>
2025-01-20 20:17:19 -0500

    test

--- frontend/my-app/src/pages/menus.js
+++ frontend/my-app/src/pages/menus.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { fetchWithAuth } from '@/utils/fetch';
+import ProtectedRoute from '@/components/ProtectedRoute';
 const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 export default function Menus() {
