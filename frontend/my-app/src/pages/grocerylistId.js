b689e3dd89f480a5861fca1db8b6723e837d9380
maxwell903 <maxwayne903@gmail.com>
2025-01-22 21:45:08 -0500

    test

--- frontend/my-app/src/pages/grocerylistId.js
+++ frontend/my-app/src/pages/grocerylistId.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import { Plus, ChevronUp, ChevronDown, Trash, Layers } from 'lucide-react';
+import { Plus, ChevronUp, ChevronDown, Trash, Layers, X, Check } from 'lucide-react';
 import { useAuth } from '@/contexts/AuthContext';
 import { fetchWithAuth } from '@/utils/fetch';
 
